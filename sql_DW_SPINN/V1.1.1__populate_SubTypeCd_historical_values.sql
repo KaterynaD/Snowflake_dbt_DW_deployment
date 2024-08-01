eed623d9cbc6f2ace8ca124378b9d82c5431e9de
Kate Drogaieva <drogaieva@gmail.com>
2024-08-01 16:55:00 -0700

    Fix in deployment scripts

--- sql_DW_SPINN/V1.1.1__populate_SubTypeCd_historical_values.sql
+++ sql_DW_SPINN/V1.1.1__populate_SubTypeCd_historical_values.sql
@@ -1,5 +1,5 @@
-update DEV_DB.DW_SPINN.DIM_POLICY
+update DIM_POLICY
 set SubTypeCd=h.SubTypeCd
-from SPINN_DB.PRODCSE.BasicPolicy as h join DEV_DB.DW_SPINN.DIM_POLICY d
+from SPINN_DB.PRODCSE.BasicPolicy as h join DIM_POLICY d
 on h.SystemId=d.policy_uniqueid;
 
